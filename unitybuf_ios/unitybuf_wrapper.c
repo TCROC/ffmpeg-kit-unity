1c68c67dbc377dda91abc024e9119647151a2af4
NON906 <mumeigames@gmail.com>
2022-01-21 19:28:19 +0900

    dl呼び出しの改善

--- unitybuf_ios/unitybuf_wrapper.c
+++ unitybuf_ios/unitybuf_wrapper.c
@@ -7,6 +7,14 @@ static void* g_dl_handle = NULL;
 static int g_open_count = 0;
 
 static int unitybuf_open(URLContext *h, const char *uri, int flags) {
+    static void* dl_handle = NULL;
+    static int (*casted_symbol)(URLContext *, const char *, int) = NULL;
+
+    if (g_dl_handle != dl_handle) {
+        casted_symbol = NULL;
+        dl_handle = g_dl_handle;
+    }
+
     if (g_dl_handle == NULL) {
         g_dl_handle = dlopen("./ffmpegkit.framework/ffmpegkit", RTLD_LAZY);
         if (g_dl_handle == NULL) {
@@ -16,32 +24,44 @@ static int unitybuf_open(URLContext *h, const char *uri, int flags) {
 
     g_open_count++;
 
-    void* symbol = dlsym(g_dl_handle, "unitybuf_open");
-    const char* const error_message = dlerror();
-    if (error_message != NULL) {
-        dlclose(g_dl_handle);
-        g_dl_handle = NULL;
-        g_open_count = 0;
-        return -1;
+    if (casted_symbol == NULL) {
+        void* symbol = dlsym(g_dl_handle, "unitybuf_open");
+        const char* const error_message = dlerror();
+        if (error_message != NULL) {
+            dlclose(g_dl_handle);
+            g_dl_handle = NULL;
+            g_open_count = 0;
+            return -1;
+        }
+        casted_symbol = symbol;
     }
-    int (*casted_symbol)(URLContext *, const char *, int) = symbol;
     return casted_symbol(h, uri, flags);
 }
 
 static int unitybuf_close(URLContext *h) {
+    static void* dl_handle = NULL;
+    static int (*casted_symbol)(URLContext *) = NULL;
+
+    if (g_dl_handle != dl_handle) {
+        casted_symbol = NULL;
+        dl_handle = g_dl_handle;
+    }
+
     if (g_dl_handle == NULL) {
         return -1;
     }
 
-    void* symbol = dlsym(g_dl_handle, "unitybuf_close");
-    const char* const error_message = dlerror();
-    if (error_message != NULL) {
-        dlclose(g_dl_handle);
-        g_dl_handle = NULL;
-        g_open_count = 0;
-        return -1;
+    if (casted_symbol == NULL) {
+        void* symbol = dlsym(g_dl_handle, "unitybuf_close");
+        const char* const error_message = dlerror();
+        if (error_message != NULL) {
+            dlclose(g_dl_handle);
+            g_dl_handle = NULL;
+            g_open_count = 0;
+            return -1;
+        }
+        casted_symbol = symbol;
     }
-    int (*casted_symbol)(URLContext *) = symbol;
     int ret = casted_symbol(h);
 
     g_open_count--;
@@ -56,36 +76,56 @@ static int unitybuf_close(URLContext *h) {
 }
 
 static int unitybuf_write(URLContext *h, const unsigned char *buf, int size) {
+    static void* dl_handle = NULL;
+    static int (*casted_symbol)(URLContext *, const unsigned char *, int) = NULL;
+
+    if (g_dl_handle != dl_handle) {
+        casted_symbol = NULL;
+        dl_handle = g_dl_handle;
+    }
+
     if (g_dl_handle == NULL) {
         return -1;
     }
 
-    void* symbol = dlsym(g_dl_handle, "unitybuf_write");
-    const char* const error_message = dlerror();
-    if (error_message != NULL) {
-        dlclose(g_dl_handle);
-        g_dl_handle = NULL;
-        g_open_count = 0;
-        return -1;
+    if (casted_symbol == NULL) {
+        void* symbol = dlsym(g_dl_handle, "unitybuf_write");
+        const char* const error_message = dlerror();
+        if (error_message != NULL) {
+            dlclose(g_dl_handle);
+            g_dl_handle = NULL;
+            g_open_count = 0;
+            return -1;
+        }
+        casted_symbol = symbol;
     }
-    int (*casted_symbol)(URLContext *, const unsigned char *, int) = symbol;
     return casted_symbol(h, buf, size);
 }
 
 static int unitybuf_read(URLContext *h, unsigned char *buf, int size) {
+    static void* dl_handle = NULL;
+    static int (*casted_symbol)(URLContext *, unsigned char *, int) = NULL;
+
+    if (g_dl_handle != dl_handle) {
+        casted_symbol = NULL;
+        dl_handle = g_dl_handle;
+    }
+
     if (g_dl_handle == NULL) {
         return -1;
     }
 
-    void* symbol = dlsym(g_dl_handle, "unitybuf_read");
-    const char* const error_message = dlerror();
-    if (error_message != NULL) {
-        dlclose(g_dl_handle);
-        g_dl_handle = NULL;
-        g_open_count = 0;
-        return -1;
+    if (casted_symbol == NULL) {
+        void* symbol = dlsym(g_dl_handle, "unitybuf_read");
+        const char* const error_message = dlerror();
+        if (error_message != NULL) {
+            dlclose(g_dl_handle);
+            g_dl_handle = NULL;
+            g_open_count = 0;
+            return -1;
+        }
+        casted_symbol = symbol;
     }
-    int (*casted_symbol)(URLContext *, unsigned char *, int) = symbol;
     return casted_symbol(h, buf, size);
 }
 
